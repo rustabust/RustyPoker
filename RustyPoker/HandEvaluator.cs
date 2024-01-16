6ab6361701588d86faed335494c063f375dfa97e
Robert Burrell <rburrell@koolspan.com>
2024-01-16 13:20:13 -0500

    begun refactoring evaluator. referencing static class underneath but that wont work since i will want to keep track of the cards internally in the new class

--- RustyPoker/HandEvaluator.cs
+++ RustyPoker/HandEvaluator.cs
@@ -13,9 +13,20 @@ namespace RustyPoker
     /// i.e. avoiding checking for straight multiple times. 
     /// we can also keep track of the best five of seven cards, etc...
     /// </summary>
-    public static class SimplePokerHandEvaluator
+    public class PokerHandEvaluator
     {
-        public static PokerHandRanks EvaluatePokerHand(List<Card> cards)
+        private List<Card> inputCards { get; set; }
+        public List<Card> BestHand { get; private set; }
+        public PokerHandRanks HandRank { get; set; }
+        public string HandDescription { get; set; }
+
+        public PokerHandEvaluator(List<Card> cards)
+        {
+            inputCards = cards.OrderBy(a => a.Number).ToList();
+            BestHand = new List<Card>();
+        }
+
+        public PokerHandRanks EvaluatePokerHand()
         {
 
             // i currently see no better way than to just go through the whole list of hands
@@ -24,16 +35,15 @@ namespace RustyPoker
             // * determine the rank of the hand
             // * determine the rank of the hand compared to other same ranked hands (9s full of 2s vs 2s full of 9s, or high card vs high card, evaluating kickers, etc)
             // * determine the name of the hand 
-            cards = cards.OrderBy(a => a.Number).ToList();
-            bool isFlush = IsFlush(cards);
-            //bool isStraight = IsStraight(cards);
 
-            if (isFlush)
+            // #pickuphere i started making functions for hand evaluation referencing the static base class but that is not going to work since I want to keep track of the cards...
+
+            if (this.isFlush)
             {
-                bool straightFlush = IsStraightFlush(cards);
+                bool straightFlush = IsStraightFlush();
                 if (straightFlush)
                 {
-                    bool isRoyalFlushResult = IsRoyalFlush(cards);
+                    bool isRoyalFlushResult = isRoyalFlush(cards);
                     if (isRoyalFlushResult)
                     {
                         return PokerHandRanks.RoyalFlush;
@@ -123,19 +133,15 @@ namespace RustyPoker
             return result;
         }
 
-        public static bool IsStraightFlush(List<Card> cards)
+        private bool isStraightFlush
         {
-            bool result = false;
-            // straight flush
-            // need to verify the flush cards are also the straight cards...
-            var flushCards = getFlushCards(cards);
-            if (flushCards != null)
+            get
             {
-                result = IsStraight(flushCards);
+                return SimplePokerHandEvaluator.IsStraightFlush(this.inputCards);
             }
-            return result;
         }
-        public static bool IsRoyalFlush(List<Card> cards)
+
+        private static bool isRoyalFlush(List<Card> cards)
         {
             bool result = IsFlush(cards);  // would be nice if is flush returned the suit
 
@@ -157,109 +163,76 @@ namespace RustyPoker
             return result;
         }
 
-        public static bool IsStraight(List<Card> cards)
+        public bool IsStraight
         {
-
-            cards = cards.OrderBy(a => a.Number).ToList();
-
-            bool result = doIsStraight(cards);
-
-            if (!result && cards.Any(a => a.Number == Numbers.Ace))
+            get
             {
-                // be default above the list is sorted with ace high
-                // reorder the cards here with ace low and check it again
-
-                var cards2 = new List<Card>();
-
-                var aces = cards.Where(a => a.Number == Numbers.Ace);
-                cards2.AddRange(aces);
-
-                cards.RemoveAll(a => a.Number == Numbers.Ace);
-                foreach (var card in cards)
-                {
-                    cards2.Add(card);
-                }
-
-                result = doIsStraight(cards2);
+                return SimplePokerHandEvaluator.IsStraight(this.inputCards); 
             }
-
-            return result;
         }
 
-        private static bool doIsStraight(List<Card> cards)
+        //private static bool doIsStraight(List<Card> cards)
+        //{
+        //    const int STRAIGHT_SEQUENCE_CARD_COUNT = 5;
+
+        //    int sequenceCounter = 0;
+
+        //    // since we're testing sequence here with i and i+1,
+        //    // only run the loop to the 2nd from last item in the array
+        //    for (int i = 0; i < cards.Count - 1; i++)
+        //    {
+        //        if (cards[i].Number == cards[i + 1].Number)
+        //        {
+        //            continue;
+        //        }
+        //        bool sequenceOk = isSequence(cards[i], cards[i + 1]);
+        //        if (sequenceOk)
+        //        {
+        //            sequenceCounter++;
+
+        //            // since we're checking if card1 and card2 are in a sequence,
+        //            // for five cards, there will only be four comparisons.
+        //            // once we get to four successful comparisons, we have a straight
+        //            if (sequenceCounter == STRAIGHT_SEQUENCE_CARD_COUNT - 1)
+        //            {
+        //                return true; // todo problem with this is that it returns yes for a straight but it might not be the highest straight in the hand.
+        //            }
+        //        }
+        //        else
+        //        {
+        //            sequenceCounter = 0;
+        //        }
+
+        //    }
+        //    return false;
+        //}
+
+        //private static bool isSequence(Card card1, Card card2)
+        //{
+        //    var number = 100;
+        //    if (card1.Number == Numbers.Ace) // i think here ace from card1 should always be considered low. if high, then card2 def isnt in sequence.
+        //    {
+        //        number = 1;
+        //    }
+        //    else
+        //    {
+        //        number = (int)card1.Number;
+        //    }
+        //    var nextNumber = (Numbers)number + 1;
+        //    return card2.Number == nextNumber;
+        //}
+
+       
+
+
+        private bool isFlush
         {
-            const int STRAIGHT_SEQUENCE_CARD_COUNT = 5;
-
-            int sequenceCounter = 0;
-
-            // since we're testing sequence here with i and i+1,
-            // only run the loop to the 2nd from last item in the array
-            for (int i = 0; i < cards.Count - 1; i++)
+            get
             {
-                if (cards[i].Number == cards[i + 1].Number)
-                {
-                    continue;
-                }
-                bool sequenceOk = isSequence(cards[i], cards[i + 1]);
-                if (sequenceOk)
-                {
-                    sequenceCounter++;
-
-                    // since we're checking if card1 and card2 are in a sequence,
-                    // for five cards, there will only be four comparisons.
-                    // once we get to four successful comparisons, we have a straight
-                    if (sequenceCounter == STRAIGHT_SEQUENCE_CARD_COUNT - 1)
-                    {
-                        return true; // todo problem with this is that it returns yes for a straight but it might not be the highest straight in the hand.
-                    }
-                }
-                else
-                {
-                    sequenceCounter = 0;
-                }
-
+                return SimplePokerHandEvaluator.IsFlush(this.inputCards);
             }
-            return false;
-        }
-        private static bool isSequence(Card card1, Card card2)
-        {
-            var number = 100;
-            if (card1.Number == Numbers.Ace) // i think here ace from card1 should always be considered low. if high, then card2 def isnt in sequence.
-            {
-                number = 1;
-            }
-            else
-            {
-                number = (int)card1.Number;
-            }
-            var nextNumber = (Numbers)number + 1;
-            return card2.Number == nextNumber;
-        }
-
-        private static List<Card>? getFlushCards(List<Card> cards)
-        {
-            List<Card>? result = null;
-            const int FLUSH_SUITED_CARD_COUNT = 5;
-            var suitGroups = cards.GroupBy(a => a.Suit);
-            foreach (var suitGroup in suitGroups)
-            {
-                if (suitGroup.Count() >= FLUSH_SUITED_CARD_COUNT)
-                {
-                    result = suitGroup.ToList(); // if more than 5, should sort them and return the highest 5?
-                }
-            }
-            return result;
-        }
-
-        public static bool IsFlush(List<Card> cards)
-        {
-            bool result = false;
-            var flushCards = getFlushCards(cards);
-            if (flushCards != null)
-            {
-                result = true;
-            }
-            return result;
         }
     }
+
+    
 }
